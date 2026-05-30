bc7dc6945d913d29918a7cc2493b548cba6eb68d
alok singh <pratapalok1310@gmail.com>
2026-05-31 00:30:32 +0530

    fix: match exact case-sensitivity for database configuration path

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('./config/dB');
 
 
 // Import Route Handlers
